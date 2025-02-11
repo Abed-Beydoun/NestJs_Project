6618223cf03178c9864069ebab8ac4fd5b3f7c53
Abed <abedbeydounaccs@gmail.com>
2025-02-12 00:10:35 +0200

    Develop | Implemented user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/models/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
+import bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
@@ -9,4 +11,39 @@ export class UsersService {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<User>,
   ) {}
+
+  async createUser(user: CreateUserDto) {
+    if (!user.firstName || user.firstName === '') {
+      return 'firstName is required';
+    }
+
+    if (!user.lastName || user.lastName === '') {
+      return 'lastName is required';
+    }
+
+    if (!user.email || user.email === '') {
+      return 'email is required';
+    }
+
+    if (!user.password || user.password === '') {
+      return 'password is required';
+    }
+
+    const userExist = await this.userModel.findOne({
+      email: user.email,
+    });
+
+    if (userExist) {
+      throw new ConflictException('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+
+    const createdUser = new this.userModel({
+      ...CreateUserDto,
+      password: hashedPassword,
+    });
+
+    return createdUser.save();
+  }
 }
