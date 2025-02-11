b1798a1c7cf1807e579307f6bd33c308a884d9f6
Abed <abedbeydounaccs@gmail.com>
2025-02-12 00:24:01 +0200

    Refactor | Fixed bcrypt import

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/models/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
-import bcrypt from 'bcryptjs';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
