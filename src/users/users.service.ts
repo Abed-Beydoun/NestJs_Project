32075d281d41b3f2ab0a6729c8e6715dce846dc7
Abed <abedbeydounaccs@gmail.com>
2025-02-11 22:11:37 +0200

    Develop | Injected the user model

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/models/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  //Inject the model that we want to use in the constructor arguments
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+}
