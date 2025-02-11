bf451ed5c2e9aadd51ae6d93e3e75bf25093c9b1
Abed <abedbeydounaccs@gmail.com>
2025-02-11 22:00:06 +0200

    Develop | Registered user schema

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controllers/users/users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, userSchema } from 'src/models/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: userSchema,
+      },
+    ]),
+  ],
   controllers: [UsersController],
 })
 export class UsersModule {}
