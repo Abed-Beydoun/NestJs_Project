b571eb5b6b7dabeeb7f2c620a8c52b33aaa258e8
Abed <abedbeydounaccs@gmail.com>
2025-02-13 00:42:45 +0200

    Develop | Added a global prefix for routes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //Set a url prefix
+  app.setGlobalPrefix('api');
   //Using validation pipes
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
