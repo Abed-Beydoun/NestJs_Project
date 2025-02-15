3341013e83a11d0d1368f94637b96da15079e248
Abed <abedbeydounaccs@gmail.com>
2025-02-15 22:34:38 +0200

    Refactor | Added required imports

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, userSchema } from 'src/models/user.schema';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from 'src/models/post.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: userSchema,
+      },
+    ]),
+    TypeOrmModule.forFeature([Post]),
+  ],
   controllers: [PostsController],
   providers: [PostsService],
 })
