1ce52f5229c9bbcaabbe65da0283fc3b78c5325d
Abed <abedbeydounaccs@gmail.com>
2025-02-15 22:35:06 +0200

    Develop | Implemented create post controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Post()
+  async createPost(@Body() post: CreatePostDto) {
+    return this.postsService.createPost(post);
+  }
 }
